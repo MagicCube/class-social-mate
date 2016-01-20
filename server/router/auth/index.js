c939a3013f26f6eff8fe842c5368f8050233c0dc
Henry Li <henry.li03@sap.com>
2016-01-20 19:49:39 +0800

    Bug fix.

--- server/router/auth/index.js
+++ server/router/auth/index.js
@@ -5,10 +5,16 @@ import app from "../../app";
 const router = Router();
 
 router.get("/login", (req, res) => {
+    let errorMessage = null;
+    if (req.session.authErrorMessage)
+    {
+        errorMessage = req.session.authErrorMessage;
+        req.session.authErrorMessage = null;
+    }
     res.render("auth/login", {
         app,
         redirectUrl: req.query.redirect ? req.query.redirect : "/",
-        authErrorMessage: req.session.authErrorMessage ? req.session.authErrorMessage : null
+        authErrorMessage: errorMessage
     });
 });
 
